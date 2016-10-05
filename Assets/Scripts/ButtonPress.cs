bbc5e578e30e8108636ebbca56108262013af968
RoarkCat <nathanwatson1990@gmail.com>
2016-10-04 20:42:44 -0500

    Adding block stuff.

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour {
+
+    public Animator blockAnimator;
+
+    private string buttonToPress = "space";
+    private int timesToPress = 3;
+
+    private int trackPresses = 0;
+
+	void Update () {
+        if (Input.GetKeyDown(buttonToPress))
+        {
+            trackPresses++;
+            if (trackPresses >= timesToPress)
+            {
+                blockAnimator.SetTrigger("explode");
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	void Update () {
+        transform.Rotate(Vector3.right * 15 * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime, Space.World);
+    }
+}
